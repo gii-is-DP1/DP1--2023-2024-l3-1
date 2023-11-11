976c9e44d259d5e5e93f4232f1b1cc563c0f760b
javnievic <javiernieto03.jnv@gmail.com>
2023-11-12 00:20:19 +0100

    feat(front & backend): user profile configuration

--- frontend/src/profile/index.js
+++ frontend/src/profile/index.js
@@ -1,15 +1,45 @@
+import { Alert } from "reactstrap";
 import "../../src/static/css/profile/profilePage.css";
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
+import tokenService from "../services/token.service";
 
 
 
 
 export default function Profile(){
-    const [currentUser,setCurrentUser] = useState(null);
+    const [currentUser, setCurrentUser] = useState([]);
+    const [message, setMessage] = useState(null);
+    const jwt = tokenService.getLocalAccessToken();
+
+    useEffect(() => {
+       
+        fetch("/api/v1/dobbleusers/current", {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${jwt}`,
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setCurrentUser(data);
+          })
+          .catch((error) => {
+            console.error("Error al obtener el usuario actual:", error);
+            setMessage("Error al obtener el usuario actual");
+          });
+      }, []);
+
     return(
     <div className="auth-page-container">
-        <h1>Perfil de usuario</h1>
+        {message ? (
+          <Alert color="primary">{message}</Alert>
+        ) : (
+          <></>
+        )}
+        <h1>Mi perfil</h1>
+        <h4>Nombre de usuario:  {currentUser.username}</h4>
+        <h4>Email:  {currentUser.email}</h4>
     </div>
     );
 
