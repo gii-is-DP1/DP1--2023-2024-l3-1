f26d4204b9406112c000bc3c372bc686ba8feb1b
javnievic <javiernieto03.jnv@gmail.com>
2023-11-11 20:43:13 +0100

    feat(fronend): started user profile

---
+++ frontend/src/profile/index.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+
+
+
+
+
+export default function Profile(){
+    const [currentUser,setCurrentUser] = useState(null);
+    return(
+    <div>
+    <h1>Perfil de usuario</h1>
+    </div>
+    );
+
+
+
+
+}
