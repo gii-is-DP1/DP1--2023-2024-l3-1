1cb5745ccf1689b410fd8f5724f9c34d8f14aa56
Fernando Fernández <ferferga@hotmail.com>
2023-11-12 20:04:52 +0100

    refactor(frontend): use correct image
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import '../App.css';
-import '../static/css/home/home.css'; 
+import '../static/css/home/home.css';
 
 export default function Home(){
     return(
         <div className="home-page-container">
-            <div className="hero-div">
-                <h1>Dobble Online</h1>
-                {/* <h3>---</h3>
-                <h3>Find the best vet for your pet</h3>  */}
-                <img alt="Dobble logo" src="logo.png" width={"30%"} height={"30%"}/>
-            </div>
         </div>
     );
 }
