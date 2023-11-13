f626cda45b513b7a12f910b0ec14d2f7d69be19b
Fernando Fernández <ferferga@hotmail.com>
2023-11-13 19:32:27 +0100

    refactor(backend): openapi details

--- src/main/java/org/springframework/samples/petclinic/configuration/OpenApiConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/OpenApiConfiguration.java
@@ -3,7 +3,6 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
@@ -11,11 +10,8 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @Configuration
 @OpenAPIDefinition(
   info =@Info(
-    title = "PetClinic APIs",
-    version = "v1.0",
-    contact = @Contact(
-      name = "DP1-2X2Y-Group Z", email = "dp1-2X2Y-groupZ@gmail.com", url = "https://dp1-2X2Y-groupZ.com"
-    ),
+    title = "Dobble Online APIs",
+    version = "v2.0",
     license = @License(
       name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"
     ),
@@ -23,6 +19,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
     description = "${api.description}"
   )
 )
+
 @SecurityScheme(
     name = "bearerAuth",
     type = SecuritySchemeType.HTTP,
