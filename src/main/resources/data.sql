a25c8f76df5069768038a3741e3c7ef064da2148
Fernando Fernández <ferferga@hotmail.com>
2023-11-13 19:27:50 +0100

    refactor(backend): add information about how to hash the passwords

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
+-- Para los hashes de las contraseñas, utilizar: https://bcrypt-generator.com/
+
 -- One admin user, named admin1 with password 4dm1n and authority admin
 INSERT INTO authorities(id,authority) VALUES (1,'ADMIN');
 INSERT INTO appusers(id,username,password,authority) VALUES (1,'admin1','$2a$10$nMmTWAhPTqXqLDJTag3prumFrAJpsYtroxf0ojesFYq0k4PmcbWUS',1);
