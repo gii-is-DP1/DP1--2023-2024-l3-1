5e5feb771e521fe303319d90de4b7e3639f2fbf6
Fernando Fernández <ferferga@hotmail.com>
2023-11-14 00:34:52 +0100

    refactor(backend): working player service and repository
    Also finish SignupRequest

--- src/main/java/org/springframework/samples/petclinic/dto/SignupRequest.java
+++ src/main/java/org/springframework/samples/petclinic/dto/SignupRequest.java
@@ -2,38 +2,18 @@ package org.springframework.samples.petclinic.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-import org.springframework.samples.petclinic.clinic.Clinic;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequest {
-	
-	// User
+	@NotBlank
+	private String email;
+
 	@NotBlank
 	private String username;
-	
-	@NotBlank
-	private String authority;
 
 	@NotBlank
 	private String password;
-	
-	//Both
-	@NotBlank
-	private String firstName;
-	
-	@NotBlank
-	private String lastName;
-	
-	@NotBlank
-	private String city;
-	//Owner
-
-	private String address;
-	private String telephone;
-	private Clinic clinic;
-
 }
