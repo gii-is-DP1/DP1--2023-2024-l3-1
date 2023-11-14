b13a6ac8fb63c2434a3244cebd5d1afc5cc18e20
Fernando Fernández <ferferga@hotmail.com>
2023-11-14 01:01:17 +0100

    refactor(backend): first buildable setup with playercontroller

--- src/main/java/org/springframework/samples/petclinic/services/PlayerService.java
+++ src/main/java/org/springframework/samples/petclinic/services/PlayerService.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.dto.SignupRequest;
 import org.springframework.samples.petclinic.model.Player;
+import org.springframework.samples.petclinic.repositories.PlayerRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,12 @@ import jakarta.validation.Valid;
 @Service
 public class PlayerService {
     private final PasswordEncoder encoder;
+	private final PlayerRepository repository;
 
 	@Autowired
-	public PlayerService(PasswordEncoder encoder) {
+	public PlayerService(PasswordEncoder encoder, PlayerRepository repository) {
 		this.encoder = encoder;
+		this.repository = repository;
 	}
 
     @Transactional
@@ -24,5 +27,10 @@ public class PlayerService {
 		player.setUsername(request.getUsername());
         player.setEmail(request.getEmail());
 		player.setPassword(encoder.encode(request.getPassword()));
+		this.repository.save(player);
     }
+
+	public Boolean existsUser(String username) {
+		return this.repository.findByUsername(username).get().getUsername().equals(username);
+	}
 }
