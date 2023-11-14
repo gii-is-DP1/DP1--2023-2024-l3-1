7bb6ceb0a89ed13250887a8f590f42868779a5df
Fernando Fernández <ferferga@hotmail.com>
2023-11-14 01:18:36 +0100

    refactor(backend): add openapi comments to new methods

--- src/main/java/org/springframework/samples/petclinic/controllers/DevelopersController.java
+++ src/main/java/org/springframework/samples/petclinic/controllers/DevelopersController.java
@@ -15,13 +15,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/v1/developers")
 @Tag(name = "Developers", description = "Información sobre los desarrolladores del sistema")
 public class DevelopersController {
+
     @GetMapping
+    @Operation(summary = "Obtiene una lista de los desarrolladores de la aplicación")
     public ResponseEntity<List<Developer>> getDevelopers(){
         MavenXpp3Reader reader = new MavenXpp3Reader();
         try {
