8c8e5eee0da945b1c2c1929606912cad8dbc8529
Joserra24 <josbanbot@alum.us.es>
2023-11-21 18:59:24 +0100

    frontend: edit and list player not finished

--- frontend/src/components/admin/PlayerListAdmin.jsx
+++ frontend/src/components/admin/PlayerListAdmin.jsx
@@ -1,115 +1,106 @@
+import { Button, Table } from "reactstrap";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Button, ButtonGroup, Table } from "reactstrap";
-import "../../static/css/admin/adminPage.css";
+import tokenService from "../../services/token.service";
+import useFetchState from "../../util/useFetchState";
 import deleteFromList from "../../util/deleteFromList";
 import getErrorModal from "../../util/getErrorModal";
+import { Link } from "react-router-dom";
 import axios from '../../services/api';
 
+
+const jwt = tokenService.localAccessToken;
+
 export default function PlayerListAdmin() {
-  const [message, setMessage] = useState(null);
-  const [players, setPlayers] = useState([]);
-  const [loading, setLoading] = useState(null);
-  const [visible, setVisible] = useState(false);
+    const [message, setMessage] = useState(null);
+    const [visible, setVisible] = useState(false);
+    const [alerts, setAlerts] = useState([]);
+    /*const [players, setPlayers] = useFetchState(
+        [],
+        `/player`,
+        jwt
+    );*/
+    const [players, setPlayers] = useState([]); // Estado para almacenar los datos de los jugadores
 
-  async function request() {
-    try {
-      setLoading(true);
-      setMessage(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/player');
+                setPlayers(response.data); // Establecer los datos de los jugadores en el estado
+            } catch (error) {
+                console.error('Error al obtener jugadores:', error);
+            }
+        };
+        fetchData();
+    }, []);
 
-      const response = await axios.get("/player");
 
-      if (response.status === 401) {
-        setMessage("Usuario actual no es administrador");
-        return;
-      } else if (response.status >= 500) {
-        setMessage("Error del servidor");
-        return;
-      }
-
-      setPlayers(response.data);
-    } catch (e) {
-      setMessage(String(e));
-    } finally {
-      setLoading(false);
-    }     
-  }
-
-  useEffect(() => {
-    const run = async () => {
-      await request();
-    }
-    run();
-  }, []);
-
-  const [alerts, setAlerts] = useState([]);
-
-  const playerList = players.map((player) => {
+    
+    const playerList =
+        players.map((p) => {
+            return (
+                <tr key={p.id}>
+                    <td className="text-center" style={{ verticalAlign: 'middle' }}>{p.profile_icon}</td>
+                    <td className="text-center" style={{ verticalAlign: 'middle' }}> {p.username} </td>
+                    <td className="text-center" style={{ verticalAlign: 'middle' }}> {p.email} </td>
+                    <td className="text-center" style={{ verticalAlign: 'middle' }}> {p.is_admin ? "ADMIN" : "PLAYER" } </td>
+                    <td className="text-center">
+                        <Link to={`/player/${p.id}`} style={{ textDecoration: "none", marginLeft: "30px" }}>
+                            <button className="auth-button-yellow">
+                                Editar
+                            </button>
+                        </Link>
+                    </td>
+                    <td className="text-center">
+                        <button
+                            className="auth-button-red"
+                            onClick={() =>
+                                deleteFromList(
+                                    `/api/v1/player/${p.id}`,
+                                    p.id,
+                                    [players, setPlayers],
+                                    [alerts, setAlerts],
+                                    setMessage,
+                                    setVisible
+                                )
+                            }
+                        >
+                            Borrar
+                        </button>
+                    </td>
+                </tr>
+            );
+        });
+    const modal = getErrorModal(setVisible, visible, message);
     return (
-      <tr key={player.id}>
-        <td>{player.profile_icon}</td>
-        <td>{player.username}</td>
-        <td>{player.email}</td>
-        <td>{player.is_admin?'ADMIN':'PLAYER'}</td>
-        <td>
-          <ButtonGroup>
-            <Button
-              size="sm"
-              color="primary"
-              aria-label={"edit-" + player.id}
-              tag={Link}
-              to={"/player/" + player.id}
-            >
-              Edit
-            </Button>
-            <Button
-              size="sm"
-              color="danger"
-              aria-label={"delete-" + player.id}
-              onClick={() =>
-                deleteFromList(
-                  `/api/v1/player/${player.id}`,
-                  player.id,
-                  [players, setPlayers],
-                  [alerts, setAlerts],
-                  setMessage,
-                  setVisible
-                )
-              }
-            >
-              Delete
-            </Button>
-          </ButtonGroup>
-        </td>
-      </tr>
-    );
-  });
-  const modal = getErrorModal(setVisible, visible, message);
-
-  return (
-    <div className="home-page-container">
-      <div className="admin-page-container">
-        <h1 className="text-center">Players</h1>
-        {alerts.map((a) => a.alert)}
-        {modal}
-        <Button color="success" tag={Link} to="/users/new">
-          Add Player
-        </Button>
         <div>
-          <Table aria-label="player" className="mt-4">
-            <thead>
-              <tr>
-                <th>Profile Icon</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>{playerList}</tbody>
-          </Table>
+            <div className="admin-page-container">
+                <h1 className="text-center" style={{ marginTop: '30px' }}>Listado Jugadores</h1>
+                {alerts.map((a) => a.alert)}
+                {modal}
+                <div>
+                    <Table aria-label="player" className="mt-4">
+                        <thead>
+                            <tr>
+                                <th className="text-center">Icono</th>
+                                <th className="text-center">Usuario</th>
+                                <th className="text-center">Correo Electrónico</th>
+                                <th className="text-center">Rol</th>
+                                <th className="text-center">Acciones</th>
+                                <th className="text-center"></th>
+                            </tr>
+                        </thead>
+                        <tbody>{playerList}</tbody>
+                    </Table>
+                    <div className="custom-button-row">
+                        <Link 
+                            to={`/player/new`}
+                            style={{ textDecoration: "none" }}
+                        > 
+                            <button className="auth-button"> Crear Jugador </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
