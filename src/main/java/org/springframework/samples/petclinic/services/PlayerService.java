5a6efb00dce6bb019f207d6bb2403eb7350639e3
SoniaRM <sonrusmor@alum.us.es>
2023-11-27 14:21:43 +0100

    listado, actualizar y borrar usuarios

--- src/main/java/org/springframework/samples/petclinic/services/PlayerService.java
+++ src/main/java/org/springframework/samples/petclinic/services/PlayerService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.dto.EditPlayerDto;
 import org.springframework.samples.petclinic.dto.SignupRequest;
+import org.springframework.samples.petclinic.model.Achievement;
 import org.springframework.samples.petclinic.model.Player;
 import org.springframework.samples.petclinic.model.enums.Icon;
 import org.springframework.samples.petclinic.repositories.PlayerRepository;
@@ -44,6 +45,7 @@ public class PlayerService {
 		return repository.findByUsername(username);
 	}
 
+	//Nos coge todos los usuarios que no son admin
 	@Transactional(readOnly = true)
 	public Optional<List<Player>> findAll() {
 		return repository.findAllNonAdmin();
@@ -128,4 +130,22 @@ public class PlayerService {
 
 		return false;
 	}
+
+	/*@Transactional(readOnly = true)
+    public Player findById(int id) {
+		Optional<Player> result=repository.findById(id);
+		return result.isPresent()?result.get():null;    
+	}*/
+	@Transactional
+    public Player savePlayer(@Valid Player newPlayer) {
+        return this.repository.save(newPlayer);
+    }
+	@Transactional
+    public void deletePlayerById(int id){
+        repository.deleteById(id);
+    }
+
+    public Optional<Player> findById(int id) {
+        return repository.findById(id);
+    }
 }
