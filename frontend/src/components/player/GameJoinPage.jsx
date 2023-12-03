2bf8a3b8d537283c868f48de349eac5b175c7804
javnievic <javiernieto03.jnv@gmail.com>
2023-12-03 19:49:55 +0100

    refactor(frontend): address review comments

--- frontend/src/components/player/GameJoinPage.jsx
+++ frontend/src/components/player/GameJoinPage.jsx
@@ -4,11 +4,6 @@ export default function GameJoinPage() {
     return (
         <div className="page-container">
             <h1>Unirme a la partida</h1>
-
-
         </div>
-
-    ); 
-
-
+    );
 }
