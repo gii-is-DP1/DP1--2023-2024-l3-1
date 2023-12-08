c6a3b38e06635dd7afca42686746c271f494f5f4
Fernando Fernández <ferferga@hotmail.com>
2023-12-08 14:03:28 +0100

    refactor(backend): create GameStatus, format icon and metric
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

---
+++ src/main/java/org/springframework/samples/petclinic/model/enums/GameStatus.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.model.enums;
+
+public enum GameStatus {
+    LOBBY,
+    STARTED,
+    FINISHED
+}
+
--- src/main/java/org/springframework/samples/petclinic/model/enums/Metric.java
+++ src/main/java/org/springframework/samples/petclinic/model/enums/Metric.java
@@ -1,6 +1,9 @@
 package org.springframework.samples.petclinic.model.enums;
 
 public enum Metric {
-    GAMES_PLAYED, VICTORIES, TOTAL_PLAY_TIME, REACTION_TIME;
+    GAMES_PLAYED,
+    VICTORIES,
+    TOTAL_PLAY_TIME,
+    REACTION_TIME
 }
 
