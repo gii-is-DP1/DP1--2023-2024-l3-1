476cc00e90a07b1724e798494512908b6c545ae5
javnievic <javiernieto03.jnv@gmail.com>
2023-12-11 17:07:07 +0100

    feat(backend): GamePlayer remodeled
    Signed-off-by: javnievic <javiernieto03.jnv@gmail.com>

---
+++ src/main/java/org/springframework/samples/petclinic/model/GamePlayer.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.model;
+
+import org.springframework.samples.petclinic.dto.PublicPlayerDto;
+import org.springframework.samples.petclinic.model.base.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "game_players")
+public class GamePlayer extends BaseEntity{
+  
+  @NotNull
+  @ManyToOne
+  @JoinColumn(name = "game_id")
+  @JsonIgnore
+  private Game game; 
+
+  
+  @NotNull
+  @ManyToOne
+  @JoinColumn(name= "player_id")
+  private Player player;
+
+   @Transient
+    @NotNull
+    public PublicPlayerDto getPlayer() {
+        return new PublicPlayerDto(this.player);
+    }
+
+}
