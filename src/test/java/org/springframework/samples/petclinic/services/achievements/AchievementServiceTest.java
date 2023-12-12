5e41371391fc5133abc2a6e188befa580a23a66e
Ruben Perez <rupg29@gmail.com>
2023-12-12 18:24:42 +0100

    AchievementTests change
    Signed-off-by: Ruben Perez <rupg29@gmail.com>

--- src/test/java/org/springframework/samples/petclinic/services/achievements/AchievementServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/services/achievements/AchievementServiceTest.java
@@ -2,9 +2,7 @@ package org.springframework.samples.petclinic.services.achievements;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
