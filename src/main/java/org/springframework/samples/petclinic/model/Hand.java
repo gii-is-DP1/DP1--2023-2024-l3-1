46fd730d9fa03aaca845b7009b0f40d2aa3adaea
javnievic <javiernieto03.jnv@gmail.com>
2023-12-27 21:20:48 +0100

    feat(backend): hand methods
    Signed-off-by: javnievic <javiernieto03.jnv@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/model/Hand.java
+++ src/main/java/org/springframework/samples/petclinic/model/Hand.java
@@ -7,6 +7,7 @@ import org.springframework.samples.petclinic.model.base.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -15,16 +16,37 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Hand extends BaseEntity{
-  
-  //TODO One to One
+public class Hand extends BaseEntity {
+
+  // TODO One to One
   @NotNull
   @OneToOne
-  private GamePlayer gamePlayer; 
+  private GamePlayer gamePlayer;
 
   @NotNull
   @OneToMany
   @Size(max = 8)
-  private List<Card> cards; 
-  
+  private List<Card> cards;
+
+  @Transient
+  @NotNull
+  public Card getCurrentCard() {
+    if (cards != null && !cards.isEmpty()) {
+      return cards.get(0);
+    } else {
+      return null;
+    }
+  }
+
+  @Transient
+  @NotNull
+  public Card getNextCard() {
+    if (cards != null && !cards.isEmpty()) {
+      cards.remove(0);
+      return cards.get(0);
+    } else {
+      return null;
+    }
+  }
+
 }
