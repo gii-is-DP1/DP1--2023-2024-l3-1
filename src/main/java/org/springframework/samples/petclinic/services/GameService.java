610c1308a1519b0b505de0e5808c0d2c1101524a
javnievic <javiernieto03.jnv@gmail.com>
2023-12-27 20:35:26 +0100

    feat(backend): GamePlayer changes
    Signed-off-by: javnievic <javiernieto03.jnv@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/services/GameService.java
+++ src/main/java/org/springframework/samples/petclinic/services/GameService.java
@@ -1,15 +1,18 @@
 package org.springframework.samples.petclinic.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.dto.GameCreateDto;
 import org.springframework.samples.petclinic.model.Game;
+import org.springframework.samples.petclinic.model.GamePlayer;
+import org.springframework.samples.petclinic.model.Player;
 import org.springframework.samples.petclinic.repositories.GameRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import jakarta.security.auth.message.AuthException;
 import jakarta.validation.Valid;
 
 @Service
@@ -58,8 +61,39 @@ public class GameService {
         return optionalGameToUpdate;
     }
 
+    @Transactional(readOnly = true)
     public Optional<List<Game>> findAll() {
         List<Game> games = gameRepository.findAll();
         return Optional.ofNullable(games);
+    }
+
+
+    @Transactional
+    public Optional<Game> addPlayerToGame(String gameId, Player player) throws AuthException {
+        Optional<Game> optionalGame = gameRepository.findById(gameId);
+
+        if (optionalGame.isPresent()) {
+            Game game = optionalGame.get();
+            Integer currentPlayer = 1;
+
+            
+            if (game.getRaw_game_players() == null) {
+                game.setRaw_game_players(new ArrayList<>());
+            }
+            
+            if (game.isOnLobby() && game.getRaw_game_players().size() + currentPlayer <= game.getMaxPlayers()) {
+                GamePlayer gamePlayer = new GamePlayer();
+                gamePlayer.setGame(game);
+                gamePlayer.setPlayer(player);
+                game.getRaw_game_players().add(gamePlayer);
+
+                //saveGame(game);
+                return Optional.of(game);
+            }
+        }else{
+            throw new AuthException("El código introducido no es correcto"+ gameId); 
+        }
+
+        return Optional.empty();
     }
 }
