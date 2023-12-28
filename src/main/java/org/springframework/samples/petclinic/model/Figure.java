a58df1ff87e8025c5ccd29863154b95bcdeb984a
javnievic <javiernieto03.jnv@gmail.com>
2023-12-28 18:32:52 +0100

    feat(backend): card entity improved and data.sql
    Signed-off-by: javnievic <javiernieto03.jnv@gmail.com>

---
+++ src/main/java/org/springframework/samples/petclinic/model/Figure.java
@@ -0,0 +1,28 @@
+package org.springframework.samples.petclinic.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.samples.petclinic.model.base.BaseEntity;
+import org.springframework.samples.petclinic.model.enums.Icon;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "figures")
+public class Figure extends BaseEntity {
+  @Enumerated(EnumType.STRING)
+  private Icon icon;
+
+  @ManyToMany(mappedBy = "figures")
+  private List<Card> cards = new ArrayList<>();
+
+}
