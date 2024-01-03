d16f73eab24f8f0faa8a0d2954214af1a4c56196
SoniaRM <sonrusmor@alum.us.es>
2024-01-03 18:49:21 +0100

    GameRepositoryTest done
    Signed-off-by: SoniaRM <sonrusmor@alum.us.es>

--- src/test/java/org/springframework/samples/petclinic/services/GameRepositoryTest.java
+++ src/test/java/org/springframework/samples/petclinic/services/GameRepositoryTest.java
@@ -1,14 +1,17 @@
 package org.springframework.samples.petclinic.services;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.model.Game;
+import org.springframework.samples.petclinic.repositories.AchievementRepository;
 import org.springframework.samples.petclinic.repositories.GameRepository;
 
 @SpringBootTest
@@ -16,12 +19,21 @@ import org.springframework.samples.petclinic.repositories.GameRepository;
 public class GameRepositoryTest {
 
   @Autowired
+  private AchievementRepository achievementRepository;
+
+  @Autowired
   private GameRepository gameRepository;
 
   @Test
-  public void testRepositoryFindAll() {
-    List<Game> achievements = gameRepository.findAll();
-    assertNotNull(achievements);
+  void testRepositoryFindAll() {
+    List<Game> games = gameRepository.findAll();
+    assertNotNull(games);
   }
 
+  @Test
+  void testFindByName() {
+    Optional<Game> game = gameRepository.findByName("partida1");
+    assertNotNull(game);
+    assertEquals("partida1", game.get().getName());
+  }
 }
