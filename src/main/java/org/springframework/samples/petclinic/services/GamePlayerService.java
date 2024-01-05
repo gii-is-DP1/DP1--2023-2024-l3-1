e0d0aae69e3401f34c4211f4769c14ec99fd545c
SoniaRM <sonrusmor@alum.us.es>
2024-01-05 12:24:09 +0100

    GameServiceTest done
    Signed-off-by: SoniaRM <sonrusmor@alum.us.es>

--- src/main/java/org/springframework/samples/petclinic/services/GamePlayerService.java
+++ src/main/java/org/springframework/samples/petclinic/services/GamePlayerService.java
@@ -1,12 +1,7 @@
 package org.springframework.samples.petclinic.services;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Game;
 import org.springframework.samples.petclinic.model.GamePlayer;
-import org.springframework.samples.petclinic.model.Player;
 import org.springframework.samples.petclinic.repositories.GamePlayerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,5 +28,9 @@ public class GamePlayerService {
     return gamePlayer;
   }
 
+  @Transactional(readOnly = true)
+  public Iterable<GamePlayer> findAll() {
+    return gamePlayerRepository.findAll();
+  }
 
 }
---
+++ src/test/java/org/springframework/samples/petclinic/services/GamePlayerServiceTest.java
@@ -0,0 +1,82 @@
+package org.springframework.samples.petclinic.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.samples.petclinic.model.Card;
+import org.springframework.samples.petclinic.model.Game;
+import org.springframework.samples.petclinic.model.GamePlayer;
+import org.springframework.samples.petclinic.model.Hand;
+import org.springframework.samples.petclinic.model.Player;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+public class GamePlayerServiceTest {
+
+  @Autowired
+  private GamePlayerService gamePlayerService;
+
+  @Autowired
+  private GameService gameService;
+
+  @Autowired
+  private PlayerService playerService;
+
+  @Autowired
+  private HandService handService;
+
+  @Autowired
+  private CardService cardService;
+
+  @Test
+  @Transactional
+  public void testSaveAchievement() {
+
+    // Contamos los GamePlayers iniciales
+    Iterable<GamePlayer> iterableGamePlayers = this.gamePlayerService.findAll();
+    int count = 0;
+    for (GamePlayer gamePlayer : iterableGamePlayers) {
+      count++;
+    }
+
+    // Inicializamos el nuevo GamePlayer
+    GamePlayer newGamePlayer = new GamePlayer();
+    // Obtenemos las propiedades necesarias
+    Optional<Game> game = gameService.findByNameGame("partida2");
+    Optional<Player> player = playerService.findPlayer(1);
+
+    // Asi creamos Hand
+    Hand newHand = new Hand();
+    List<Card> cards = this.cardService.findAll().get();
+    List<Card> listCards = cards.subList(0, 3);
+    newHand.setCards(listCards);
+    handService.saveHand(newHand);
+
+    // Establecemos las propiedades obtenidas antes en el nuevo GamePlayer
+    newGamePlayer.setGame(game.get());
+    newGamePlayer.setPlayer(player.get());
+    newGamePlayer.setHand(newHand);
+
+    // Guardamos el nuevo GamePlayer
+    gamePlayerService.saveGamePlayer(newGamePlayer);
+
+    // Volvemos a contar los GamePlayers
+    Iterable<GamePlayer> iterableGamePlayersWithNewGamePlayer = this.gamePlayerService.findAll();
+    int finalCount = 0;
+    for (GamePlayer gamePlayer : iterableGamePlayersWithNewGamePlayer) {
+      finalCount++;
+    }
+
+    // Deberia de haber un elemento más que al principio
+    assertEquals(count + 1, finalCount);
+  }
+
+}
