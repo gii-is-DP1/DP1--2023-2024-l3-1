caa4ceacd006e3408d9b8878dc6406fff190c528
javnievic <javiernieto03.jnv@gmail.com>
2024-01-09 21:24:43 +0100

    chore(backend): code refactoring
    Signed-off-by: javnievic <javiernieto03.jnv@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/services/GamePlayerService.java
+++ src/main/java/org/springframework/samples/petclinic/services/GamePlayerService.java
@@ -1,12 +1,8 @@
 package org.springframework.samples.petclinic.services;
 
-import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Game;
 import org.springframework.samples.petclinic.model.GamePlayer;
-import org.springframework.samples.petclinic.model.Player;
 import org.springframework.samples.petclinic.repositories.GamePlayerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,15 +11,12 @@ import jakarta.validation.Valid;
 
 @Service
 public class GamePlayerService {
-  private final GameService gameService;
-  private final PlayerService playerService;
+
   private final GamePlayerRepository gamePlayerRepository;
 
   @Autowired
-  public GamePlayerService(GameService gameService, PlayerService playerService,
-      GamePlayerRepository gamePlayerRepository) {
-    this.gameService = gameService;
-    this.playerService = playerService;
+  public GamePlayerService(GamePlayerRepository gamePlayerRepository) {
+
     this.gamePlayerRepository = gamePlayerRepository;
   }
 
