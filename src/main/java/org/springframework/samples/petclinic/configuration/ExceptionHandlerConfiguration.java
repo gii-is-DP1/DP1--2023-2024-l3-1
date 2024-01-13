139667d1778d3f0f6cea2912d99ee5dee132ca98
Fernando Fernández <ferferga@hotmail.com>
2024-01-13 05:26:05 +0100

    refactor: print stacktrace in screen as well
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- src/main/java/org/springframework/samples/petclinic/configuration/ExceptionHandlerConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/ExceptionHandlerConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -25,10 +26,12 @@ public class ExceptionHandlerConfiguration {
 
     public ExceptionHandlerConfiguration(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
+        this.objectMapper.setSerializationInclusion(Include.ALWAYS);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception ex, HttpServletResponse response) throws IOException {
+        ex.printStackTrace();
         String jsonBody = objectMapper.writeValueAsString(Map.of("data", ex.getLocalizedMessage()));
         return new ResponseEntity<String>(jsonBody, HttpStatus.INTERNAL_SERVER_ERROR);
     }
