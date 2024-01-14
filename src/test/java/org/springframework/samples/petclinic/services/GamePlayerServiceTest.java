0c5c790a7ed81e6c729588dc1cc74cee693c4648
Joserra24 <josbanbot@alum.us.es>
2024-01-14 12:21:51 +0100

    Final Test
    Signed-off-by: Joserra24 <josbanbot@alum.us.es>

--- src/test/java/org/springframework/samples/petclinic/services/GamePlayerServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/services/GamePlayerServiceTest.java
@@ -1,76 +1,86 @@
 package org.springframework.samples.petclinic.services;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.samples.petclinic.model.Card;
 import org.springframework.samples.petclinic.model.Game;
 import org.springframework.samples.petclinic.model.GamePlayer;
 import org.springframework.samples.petclinic.model.Player;
-
-import jakarta.transaction.Transactional;
+import org.springframework.samples.petclinic.repositories.GamePlayerRepository;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
 public class GamePlayerServiceTest {
 
-  @Autowired
+  @Mock
+  private GamePlayerRepository gamePlayerRepository;
+
+  @InjectMocks
   private GamePlayerService gamePlayerService;
 
-  @Autowired
-  private GameService gameService;
+  @Test
+  void testSave() {
+    GamePlayer gamePlayer = new GamePlayer();
+    when(gamePlayerRepository.save(gamePlayer)).thenReturn(gamePlayer);
 
-  @Autowired
-  private PlayerService playerService;
+    GamePlayer savedGamePlayer = gamePlayerService.save(gamePlayer);
 
-  @Autowired
-  private CardService cardService;
+    verify(gamePlayerRepository).save(gamePlayer);
+    assertEquals(gamePlayer, savedGamePlayer);
+  }
 
   @Test
-  @Transactional
-  public void testSaveGamePlayer() {
-    // Contamos los GamePlayers iniciales
-    //Iterable<GamePlayer> iterableGamePlayers = this.gamePlayerService.findAll();
-    int count = 0;
-    // for (GamePlayer gamePlayer : iterableGamePlayers) {
-    //   count++;
-    // }
+  void testCreateGamePlayerCreator() {
+    Player player = new Player();
 
-    // Inicializamos el nuevo GamePlayer
-    GamePlayer newGamePlayer = new GamePlayer();
-    // Obtenemos las propiedades necesarias
-    Optional<Game> game = gameService.findByNameGame("partida2");
-    Optional<Player> player = playerService.findPlayer(1);
+    GamePlayer gamePlayer = gamePlayerService.createGamePlayerCreator(player);
 
-    // Asi creamos Hand
-    // Hand newHand = new Hand();
-    // List<Card> cards = this.cardService.findAll().get();
-    // List<Card> listCards = cards.subList(0, 3);
-    // newHand.setCards(listCards);
-    // handService.saveHand(newHand);
-
-    // Establecemos las propiedades obtenidas antes en el nuevo GamePlayer
-    newGamePlayer.setGame(game.get());
-    newGamePlayer.setPlayer(player.get());
-    // newGamePlayer.setHand(newHand);
-
-    // Guardamos el nuevo GamePlayer
-    gamePlayerService.save(newGamePlayer);
-
-    // Volvemos a contar los GamePlayers
-    // Iterable<GamePlayer> iterableGamePlayersWithNewGamePlayer = this.gamePlayerService.findAll();
-    int finalCount = 0;
-    // for (GamePlayer gamePlayer : iterableGamePlayersWithNewGamePlayer) {
-    //   finalCount++;
-    // }
-
-    // Deberia de haber un elemento más que al principio
-    assertEquals(count + 1, finalCount);
+    assertNotNull(gamePlayer);
+    assertNull(gamePlayer.getGame());
+    assertEquals(player, gamePlayer.getPlayer());
   }
+
+  @Test
+  void testCreateGamePlayer() {
+    Player player = new Player();
+    Game game = new Game();
+
+    when(gamePlayerRepository.save(any(GamePlayer.class))).thenAnswer(invocation -> {
+      GamePlayer savedGamePlayer = invocation.getArgument(0);
+      savedGamePlayer.setId(1);
+      return savedGamePlayer;
+    });
+
+    GamePlayer gamePlayer = gamePlayerService.createGamePlayer(player, game);
+
+    assertNotNull(gamePlayer);
+    assertEquals(player, gamePlayer.getPlayer());
+    assertEquals(game, gamePlayer.getGame());
+    assertNotNull(gamePlayer.getId());
+  }
+
+  @Test
+  void testGetGamePlayerByUsernameAndGame() {
+    String username = "testUser";
+    Game game = new Game();
+    when(gamePlayerRepository.findGamePlayerByUsernameAndGame(username, game.getId()))
+        .thenReturn(Optional.of(new GamePlayer()));
+
+    Optional<GamePlayer> result = gamePlayerService.getGamePlayerByUsernameAndGame(username, game);
+
+    assertTrue(result.isPresent());
+  }
+
 }
--- src/test/java/org/springframework/samples/petclinic/services/GameRepositoryTest.java
+++ src/test/java/org/springframework/samples/petclinic/services/GameRepositoryTest.java
@@ -2,6 +2,8 @@ package org.springframework.samples.petclinic.services;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.model.Game;
 import org.springframework.samples.petclinic.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -27,9 +30,41 @@ public class GameRepositoryTest {
   }
 
   @Test
+  @Transactional
   void testFindByName() {
-    Optional<Game> game = gameRepository.findByName("partida1");
-    assertNotNull(game);
-    assertEquals("partida1", game.get().getName());
+    String gameName = "exampleGame";
+    Game game = new Game();
+    game.setName(gameName);
+    game.setMax_players(2);
+    gameRepository.save(game);
+
+    Optional<Game> foundGame = gameRepository.findByName(gameName);
+
+    assertTrue(foundGame.isPresent());
+    assertEquals(gameName, foundGame.get().getName());
+  }
+
+  @Test
+  void testFindByNameNonExistent() {
+    String nonExistentGameName = "nonExistentGame";
+    Optional<Game> foundGame = gameRepository.findByName(nonExistentGameName);
+
+    assertFalse(foundGame.isPresent());
+  }
+
+  @Test
+  public void testFindGamesByPlayerId() {
+    Integer playerId = 1;
+    Optional<List<Game>> gamesByPlayerId = gameRepository.findGamesByPlayerId(playerId);
+    assertTrue(gamesByPlayerId.isPresent());
+    assertEquals(0, gamesByPlayerId.get().size());
+  }
+
+  @Test
+  public void testFindGamesByPlayerUsername() {
+    String username = "username";
+    Optional<List<Game>> gamesByPlayerUsername = gameRepository.findGamesByPlayerUsername(username);
+    assertTrue(gamesByPlayerUsername.isPresent());
+    assertEquals(0, gamesByPlayerUsername.get().size());
   }
 }
